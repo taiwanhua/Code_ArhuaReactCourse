821b86baeb1d7f26c9490ebe722b4de54c5fb067
Arhua <a0987837233@gmail.com>
2020-04-05 02:00:35 +0800

    JSX語法介紹

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React from 'react';
 import Buttonone from './Buttonone'
 
 const App = () => {
+  let number = 1;
+  let trueOrfalse = true;
+  let list = [{ ID: number, Name: "阿華" }, { ID: number + 1, Name: "阿花" }, { ID: number + 2, Name: "阿話" }];
   return (
     <>
       <p>這是我第一個函數組件App</p>
-      <Buttonone></Buttonone>
+      {trueOrfalse && list.map((item) => (
+        <ul key={item.ID}>
+          <li>ID: {item.ID}</li>
+          <li>Name: {item.Name}</li>
+        </ul>
+      ))}
+
+
     </>
   )
 }
