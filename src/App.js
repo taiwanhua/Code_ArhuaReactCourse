08d4fb18049b0f00cb8b9712a5c212fe9b066e1d
Arhua <a0987837233@gmail.com>
2020-04-06 01:34:38 +0800

    使用基本CSS樣式

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import Buttonone from './Buttonone'
+import './App.css'
 
 const App = () => {
-  let number = 1;
-  let trueOrfalse = true;
-  let list = [{ ID: number, Name: "阿華" }, { ID: number + 1, Name: "阿花" }, { ID: number + 2, Name: "阿話" }];
+
   return (
     <>
-      <p>這是我第一個函數組件App</p>
-      {trueOrfalse && list.map((item) => (
-        <ul key={item.ID}>
-          <li>ID: {item.ID}</li>
-          <li>Name: {item.Name}</li>
-        </ul>
-      ))}
-
+      <p id="myapp" className={"myclass myclass2"}>這是我第一個函數組件App</p>
 
     </>
   )
