51d893d4cbb5d31b37e8b08e6eda7bb322c6dc74
Arhua <a0987837233@gmail.com>
2020-04-11 17:31:41 +0800

    組件自己使用state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'
 import Buttonone from './Buttonone'
 
 const App = () => {
+  const [Count, setCount] = useState(0);
 
   return (
     <>
       <p >這是我第一個函數組件App</p>
-      <Buttonone p1={[2, 1]} p2={{ c: 3 }}>
-        <div>x</div>
-        y
-      </Buttonone>
+      <button onClick={() => { setCount(Count + 1) }}>APP組件自己的按紐</button>
+      <div>APP組件自己的按紐，按了 : {Count}次</div>
+      <div style={{ backgroundColor: Count > 0 ? "green" : "red" }}>按了變色</div>
     </>
   )
 }
