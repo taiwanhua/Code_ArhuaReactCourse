a6eaf96bb01d03456a607b1454c895f175ba49b4
Arhua <a0987837233@gmail.com>
2020-04-12 12:55:42 +0800

    用傳入的props做組件的state

--- src/Buttonone.js
+++ src/Buttonone.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const Buttonone = (props) => {
-
+    const [Restore, setRestore] = useState(props.a)
     return (
-        <button>按鈕 {props.a}</button>
+        <>
+            <button onClick={() => { setRestore(Restore + 2) }}> 按鈕 {Restore}</button>
+            <p>props.a : {props.a}</p>
+        </>
     )
 }
 
