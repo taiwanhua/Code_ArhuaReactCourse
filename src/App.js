5586029d0dce9c43786d553c1078c7e9d04239af
Arhua <a0987837233@gmail.com>
2020-04-22 01:30:52 +0800

    不傳DependencyList

--- src/App.js
+++ src/App.js
@@ -1,64 +1,69 @@
-// import React, { useState } from 'react';
-// import './App.css'
-// import Buttonone from './Buttonone'
-// import Buttontwo from './Buttontwo';
-// import { Context } from './store'
-
-// const App = () => {
-//   const [Count, setCount] = useState(0);
-
-//   return (
-//     <>
-//       <p >這是我第一個函數組件App</p>
-//       <Context.Provider value={{ Count, setCount }}>
-//         <Buttonone></Buttonone>
-//         <Buttontwo></Buttontwo>
-//       </Context.Provider>
-//     </>
-//   )
-// }
-
-// export default App;
-
-
-import React, { Component } from 'react';
-import Buttonone from './Buttonone'
+import React, { useEffect, useState } from 'react';
 import Buttontwo from './Buttontwo';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    console.log("執行App的constructor函數");
-    this.state = { count: 0, unmount: false };
-  }
-  componentDidMount() {
-    console.log("執行App的componentDidMount方法");
-    console.log(document.getElementById("pForApp"));
-  }
-  componentDidUpdate() {
-    console.log("執行App的componentDidUpdate方法，count : " + this.state.count);
-  }
-  componentWillUnmount() {
-  }
-  updatecount() {
-    this.setState({ ...this.state, count: this.state.count + 1 });
-    console.log("觸發App的updatecount，按下按鈕時的count值 : ", this.state.count)
-  }
-  unmountButtonone() {
-    this.setState({ ...this.state, unmount: !this.state.unmount });
-  }
-  render() {
-    console.log("執行App的render函數，按下按鈕時的count值 : ", this.state.count)
-    return (
-      <>
-        <p id="pForApp" >這是我第一個函數組件App</p>
-        <button onClick={() => { this.updatecount() }}>觸發App重新渲染次數 : {this.state.count}</button>
-        <button onClick={() => { this.unmountButtonone() }}>卸載/掛載Buttonone </button>
-        {!this.state.unmount && <Buttonone></Buttonone>}
-        <Buttontwo></Buttontwo>
-      </>
-    );
-  }
+const App = (props) => {
+
+  const [State, setState] = useState({ show: true, count: 0 });
+
+  useEffect(() => {
+    console.log("App的EffectCallback")
+
+    // return () => {
+    //   console.log("App的cleanup function")
+    // }
+  })
+
+  return (
+    <>
+      <p>這是我第一個函數組件App</p>
+      <button onClick={() => { setState({ ...State, show: !State.show }) }}>卸載/掛載</button>
+      <button onClick={() => { setState({ ...State, count: State.count + 1 }) }}>觸發App重新渲染次數 : {State.count}</button>
+      {State.show && <Buttontwo></Buttontwo>}
+    </>
+  )
 }
 
 export default App;
+
+
+// import React, { Component } from 'react';
+// import Buttonone from './Buttonone'
+// import Buttontwo from './Buttontwo';
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     console.log("執行App的constructor函數");
+//     this.state = { count: 0, unmount: false };
+//   }
+//   componentDidMount() {
+//     console.log("執行App的componentDidMount方法");
+//     console.log(document.getElementById("pForApp"));
+//   }
+//   componentDidUpdate() {
+//     console.log("執行App的componentDidUpdate方法，count : " + this.state.count);
+//   }
+//   componentWillUnmount() {
+//   }
+//   updatecount() {
+//     this.setState({ ...this.state, count: this.state.count + 1 });
+//     console.log("觸發App的updatecount，按下按鈕時的count值 : ", this.state.count)
+//   }
+//   unmountButtonone() {
+//     this.setState({ ...this.state, unmount: !this.state.unmount });
+//   }
+//   render() {
+//     console.log("執行App的render函數，按下按鈕時的count值 : ", this.state.count)
+//     return (
+//       <>
+//         <p id="pForApp" >這是我第一個函數組件App</p>
+//         <button onClick={() => { this.updatecount() }}>觸發App重新渲染次數 : {this.state.count}</button>
+//         <button onClick={() => { this.unmountButtonone() }}>卸載/掛載Buttonone </button>
+//         {!this.state.unmount && <Buttonone></Buttonone>}
+//         <Buttontwo></Buttontwo>
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
--- src/Buttontwo.js
+++ src/Buttontwo.js
@@ -1,49 +1,58 @@
-// import React from 'react';
-// import { Context } from './store';
+import React, { useEffect, useState } from 'react';
 
-// const Buttontwo = () => {
-//     return (
-//         <>
-//             <Context.Consumer>
-//                 {value => <p>Buttontwo組件 : {value.Count}</p>}
-//             </Context.Consumer>
-//         </>
-//     )
-// }
+const Buttontwo = (props) => {
 
-// export default Buttontwo;
+    const [State, setState] = useState({ show: true, count: 0 });
 
+    useEffect(() => {
+        console.log("Buttontwo的EffectCallback")
 
-import React, { Component } from 'react';
+        // return () => {
+        //     console.log("Buttontwo的cleanup function")
+        // }
+    })
 
-class Buttontwo extends Component {
-    constructor(props) {
-        super(props);
-        console.log("執行Buttontwo的constructor函數");
-        this.state = { count: 0 };
-    }
-    componentDidMount() {
-        console.log("執行Buttontwo的componentDidMount方法");
-        console.log(document.getElementById("pForButtontwo"));
-    }
-    componentDidUpdate() {
-        console.log("執行Buttontwo的componentDidUpdate方法，count : " + this.state.count);
-    }
-    componentWillUnmount() {
-    }
-    updatecount() {
-        this.setState({ count: this.state.count + 1 });
-        console.log("觸發Buttontwo的updatecount，按下按鈕時的count值 : ", this.state.count)
-    }
-    render() {
-        console.log("執行Buttontwo的render函數，按下按鈕時的count值 : ", this.state.count)
-        return (
-            <>
-                <p id="pForButtontwo" >Buttontwo組件</p>
-                <button onClick={() => { this.updatecount() }}>觸發Buttontwo重新渲染次數 : {this.state.count}</button>
-            </>
-        );
-    }
+    return (
+        <>
+            <p>Buttontwo組件</p>
+            <button onClick={() => { setState({ ...State, count: State.count + 1 }) }}>觸發Buttontwo重新渲染次數 : {State.count}</button>
+        </>
+    )
 }
 
 export default Buttontwo;
+
+
+// import React, { Component } from 'react';
+
+// class Buttontwo extends Component {
+//     constructor(props) {
+//         super(props);
+//         console.log("執行Buttontwo的constructor函數");
+//         this.state = { count: 0 };
+//     }
+//     componentDidMount() {
+//         console.log("執行Buttontwo的componentDidMount方法");
+//         console.log(document.getElementById("pForButtontwo"));
+//     }
+//     componentDidUpdate() {
+//         console.log("執行Buttontwo的componentDidUpdate方法，count : " + this.state.count);
+//     }
+//     componentWillUnmount() {
+//     }
+//     updatecount() {
+//         this.setState({ count: this.state.count + 1 });
+//         console.log("觸發Buttontwo的updatecount，按下按鈕時的count值 : ", this.state.count)
+//     }
+//     render() {
+//         console.log("執行Buttontwo的render函數，按下按鈕時的count值 : ", this.state.count)
+//         return (
+//             <>
+//                 <p id="pForButtontwo" >Buttontwo組件</p>
+//                 <button onClick={() => { this.updatecount() }}>觸發Buttontwo重新渲染次數 : {this.state.count}</button>
+//             </>
+//         );
+//     }
+// }
+
+// export default Buttontwo;
