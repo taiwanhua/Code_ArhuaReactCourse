de08a72d908c55a46d7af07db9ecf8b160978c6f
Arhua <a0987837233@gmail.com>
2020-05-02 11:47:49 +0800

    404頁面處理與useRouteMatch取得路由資訊

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Switch, Route, useParams } from 'react-router-dom';
+import { Switch, Route, useParams, useRouteMatch } from 'react-router-dom';
 
 function App() {
   return (
     <Switch>
       <Route exact path={"/"} children={<Home />} />
       <Route path={"/work/:number"} children={<Work />} />
+      <Route path={"*"} component={NotFound}></Route>
     </Switch>
   );
 }
@@ -29,6 +30,17 @@ const Work = ({ props }) => {
   return (
     <>
       <p>這裡是Work : {number}</p>
+    </>
+  )
+}
+
+const NotFound = (props) => {
+
+  const { path, url } = useRouteMatch();
+
+  return (
+    <>
+      Not Found ,path : {path} , url : {url}
     </>
   )
 }
